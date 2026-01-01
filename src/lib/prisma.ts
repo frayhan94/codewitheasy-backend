2f97440a931aab615532296355dc720c3490fcc4
Faris <frayhan94@gmail.com>
2026-01-01 19:31:13 +0700

    Add Prisma Accelerate extension and update related configurations
    Enabled the `@prisma/extension-accelerate` for performance improvements. Updated Prisma client initialization, scripts, and schema to support the new extension. Adjusted dependencies and included `directUrl` in the Prisma datasource for direct DB access.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: ReturnType<typeof createPrismaClient> };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
+}
+
+export const prisma = globalForPrisma.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
